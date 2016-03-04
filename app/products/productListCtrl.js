08f4bd7042378264ac008db72a3a3c38aefd2126
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2016-03-04 18:40:49 +0100

    Added ProductListCtrl in project.

---
+++ app/products/productListCtrl.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Blagojce on 04.3.2016.
+ */
+
+(function(){
+    "use strict";
+    angular.module("productManagement").controller("ProductListCtrl",
+    ProductListCtrl);
+
+    function ProductListCtrl (){
+
+    }
+}());
