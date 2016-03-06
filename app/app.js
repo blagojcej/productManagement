815c894b749cdfa07ff76a2a2e7395c88204c442
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2016-03-06 18:17:00 +0100

    Added routing to the project.

--- app/app.js
+++ app/app.js
@@ -4,5 +4,29 @@
 
 (function () {
     "use strict";
-    var app = angular.module("productManagement", ["common.services", "productResourceMock"])
+    var app = angular.module("productManagement", ["common.services", "ui.router", "productResourceMock"])
+
+    app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/");
+
+            $stateProvider
+                //Home
+                .state("home", {
+                    url: "/",
+                    templateUrl:"app/welcomeView.html"
+                })
+                //Products
+                .state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",
+                    controller: "ProductListCtrl as vm"
+                })
+                //Edit Product
+                .state("productEdit",{
+                    url:"products/edit/:productId",
+                    templateUrl: "app/products/productEditView.html",
+                    controller: "ProductEditCtrl as vm"
+                })
+        }]
+    );
 }());
