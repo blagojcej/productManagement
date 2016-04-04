232a9db34a613383dfa5aac3723fdf22307fc05f
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2016-04-04 07:16:07 +0200

    Added validation messages.

--- app/products/ProductEditCtrl.js
+++ app/products/ProductEditCtrl.js
@@ -5,9 +5,9 @@
 (function(){
     "use strict";
 
-    angular.module("productManagement").controller("ProductEditCtrl", ["product", ProductEditCtrl]);
+    angular.module("productManagement").controller("ProductEditCtrl", ["product", "$state", ProductEditCtrl]);
 
-    function ProductEditCtrl(product){
+    function ProductEditCtrl(product, $state) {
         var vm=this;
 
         vm.product=product;
@@ -25,5 +25,30 @@
 
             vm.opened = !vm.opened;
         };
+
+        vm.submit = function () {
+            vm.product.$save(function (data) {
+                toastr.success("Save Successful");
+            })
+        };
+
+        vm.cancel = function () {
+            $state.go('productList');
+        }
+
+        vm.addTags = function (tags) {
+            if (tags) {
+                var array = tags.split(',');
+                vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
+                vm.newTags = "";
+            }
+            else {
+                alert("Please enter one or more tags separated by commas");
+            }
+        }
+
+        vm.removeTag = function (idx) {
+            vm.product.tags.splice(idx, 1);
+        }
     }
 }());
